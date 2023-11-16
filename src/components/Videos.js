122546c11aba577f872e43673109e11c2945e8dd
Sean O'Dell <sean.odell@skejjo.dev>
2023-11-16 11:13:43 -0600

    update video.js code - update iframe height

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -52,7 +52,7 @@ function Videos() {
                         Learn more about from watching demonistrations and talks.
                     </Typography>
                 </Box>
-<iframe width="100%" height="315" src="https://www.youtube.com/embed/PlV5SH5UX1c?si=SGyIZmP1iXWG6tie" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<iframe width="100%" height="500" src="https://www.youtube.com/embed/PlV5SH5UX1c?si=SGyIZmP1iXWG6tie" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </Box>
         </Container>
     );
